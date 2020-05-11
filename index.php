f4b5ecf34b9f1b01234c7cd8fe44f69ee9c8cc75
guttyar-2213 <guttyar2213.00@gmail.com>
2020-05-11 21:18:17 +0900

    get device list test

---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+  <head><meta charset=utf-8></head>
+  <body>
+    <video playsinline autoplay></video>
+    <script>
+console.log(await navigator.mediaDevices.enumerateDevices())
+    </script>
+  </body>
+</html>
